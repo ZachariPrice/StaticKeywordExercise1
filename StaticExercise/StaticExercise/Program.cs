2f9c48c0a3e8219acad7c94e22aa085af99c0a18
ZachariPrice <129778457+ZachariPrice@users.noreply.github.com>
2023-04-26 00:50:40 -0400

    Add files via upload

---
+++ StaticExercise/StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(75); // 23.88888888888889
+            var fahrenheit = TempConverter.CelciusToFahrenheit(23.88888888888889);
+
+            Console.WriteLine($"celcius {celcius}");
+
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
